457ec0d5fe363030dfa2f4c4b03364c4fdf7919f
Voda Darian <darian.voda00@e-uvt.ro>
2022-04-25 15:11:00 +0300

    Solved exercise4.cpp

--- exercise4.cpp
+++ exercise4.cpp
@@ -10,11 +10,15 @@ Using std::set:
 
 #include <iostream>
 #include <set>
+#include <vector>
+#include <list>
+#include <map>
 
 class Trabant{
 public:
     std::string brand_, model_;
     int profit_;
+    int productionYear_{};
 
     Trabant(std::string b, std::string m, int prf){
         this->brand_ = b;
@@ -29,11 +33,15 @@ public:
         return (leftStr < rightStr);
     }
     friend std::ostream& operator<<(std::ostream& os, const Trabant& obj);
+
+    void setYear(int year){
+        this->productionYear_ = year;
+    }
 };
 
 std::ostream& operator<<(std::ostream& os, const Trabant& obj)
 {
-    os<<obj.brand_<<" :: "<<obj.model_<<" :: "<<obj.profit_<<std::endl;
+    os << obj.brand_ << " " << obj.model_ << " " << obj.profit_ << " " << obj.productionYear_ << std::endl;
     return os;
 }
 
@@ -53,6 +61,19 @@ int main()
     Trabant t10 = Trabant("Subaru", "205", 80000);
     Trabant t11 = Trabant("Ferrari", "M23", 200000);
     Trabant t12 = Trabant("Lamborghini", "Gallardo", 700000);
+
+    t1.setYear(2000);
+    t2.setYear(2007);
+    t3.setYear(2004);
+    t4.setYear(1997);
+    t5.setYear(2012);
+    t6.setYear(2020);
+    t7.setYear(2022);
+    t8.setYear(2022);
+    t9.setYear(2010);
+    t10.setYear(1945);
+    t11.setYear(1999);
+    t12.setYear(1989);
 
     all_cars.insert(t1);
     all_cars.insert(t2);
@@ -77,4 +98,46 @@ int main()
     if(k==0){
         std::cout<<"No we don't have a vericu with BMW"<<std::endl;
     }
+
+    std::vector<std::set<int>> v1;
+    std::set<int> years;
+    std::set<std::string> models;
+    for (const auto & all_car : all_cars) {
+        years.insert(all_car.productionYear_);
+    }
+
+    v1.push_back(years);
+    int x = 0;
+    for (auto i: v1){
+        for(auto j: i){
+            auto it = i.begin();
+            std::advance(it, x);
+            std::cout<<"Year: "<<*it;
+            for (auto p = all_cars.begin(); p != all_cars.end(); p++) {
+                if(p->productionYear_ == *it){
+                    std::cout<<", "<<p->brand_;
+                }
+            }
+            std::cout<<std::endl;
+            x++;
+        }
+    }
+
+    int max = 0, bestYear;
+    for (const auto & all_car : all_cars) {
+        if(max < all_car.profit_);
+        {
+            max = all_car.profit_;
+            bestYear = all_car.productionYear_;
+        }
+    }
+
+    std::cout<<"Best production year: "<<bestYear<<", with a profit of: "<<max;
+
+
+
+
+
+
+
 }
